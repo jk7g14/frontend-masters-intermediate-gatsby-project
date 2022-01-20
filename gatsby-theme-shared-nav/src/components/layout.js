c4a2e8cbd93ba72b568a4c27086b935bfbbae0b8
James <jk7g14@gmail.com>
2022-01-21 01:00:21 +0900

    add nav

--- gatsby-theme-shared-nav/src/components/layout.js
+++ gatsby-theme-shared-nav/src/components/layout.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
+import { Nav } from './nav.js';
 
 import '../styles/variables.css';
 import '../styles/global.css';
 
-import { content, footer } from '../stlyes/layout.module.css';
+import { content, footer } from '../styles/layout.module.css';
 
 export default function Layout({ children }) {
-  retrun(
+  return (
     <>
+      <Nav />
       <main className={content}>{children}</main>
       <footer className={footer}>Built with Shared Nav</footer>
-    </>,
+    </>
   );
 }
--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,5 +1,19 @@
 module.exports = {
+  siteMetadata: {
+    title: 'My Book Club',
+    navItems: [
+      {
+        label: 'books',
+        path: '/books',
+      },
+      {
+        label: 'authors',
+        path: '/authors',
+      },
+    ],
+  },
   plugins: [
+    'gatsby-theme-shared-nav',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
