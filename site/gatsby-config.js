e7d6b1764b7d11846f61395fad216866ae0a0283
Jaehun Kim <llyppp@revhat.com>
2022-01-26 21:34:03 +0900

    hotfix

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -17,10 +17,10 @@ module.exports = {
     ],
   },
   plugins: [
-    'gatsby-theme-shared-nav',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-theme-shared-nav',
     'gatsby-plugin-netlify',
   ],
 };
